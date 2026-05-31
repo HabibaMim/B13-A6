3a1e8e5facc4e872d8ea0181f6a6a91de6353c65
habibamim <habibakumkum02@gmail.com>
2026-05-31 18:08:21 +0600

    added remove functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,22 +5,36 @@ const Cart = ({carts, setCarts}) => {
 
     const handlePayment = () =>{
         setCarts([])
+        }
+
+         const handleDelete = (item) => {
+            const filteredArray =carts.filter((c) => c.id !== item.id);
+            setCarts(filteredArray);
     }
   return (
     <div className=' mt-[40px] mb-[120px] flex justify-center items-center'>
         <div className='w-[75%] border border-gray-200 rounded-lg p-[40px]'>
-            <div className='space-y-5 '>
-            <div className='text-[24px] font-bold'>Your Cart</div>
-      {carts.map(item => <div className='bg-[#F9FAFC] rounded-l-lg' key={item.id}>
+            
+            <div className='text-[24px] font-bold pb-[16px]'>Your Cart</div>
+
+            {
+                carts.length === 0 ? <p>Cart is empty.</p> : <>
+                   <div className='space-y-5 '>
+                    
+      {carts.map(item =><div className='flex justify-between items-center p-[20px] bg-[#F9FAFC] rounded-l-lg'> <div key={item.id}>
         <div className='flex gap-[20px]'><div><img className='w-[60px] h-[60px]' src={item.icon} alt="" /></div>
         <div><p className='font-bold'>{item.name}</p>
         <p className='text-gray-600'>${item.price}</p></div>
-      </div></div>)}
+      </div></div>
+      <button onClick={()=> handleDelete(item)} className='text-red-600 font-bold'>Remove</button></div>)}
       <div className='flex justify-between items-center'>
         <p className='text-gray-600'>Total:</p>
         <p className='text-[24px] font-bold'>${totalPrice}</p></div>
       </div>
       <button onClick={handlePayment} className='btn bg-linear-to-r from-[#4f39f6] to-[#9514fa] w-full text-white rounded-full'>Proceed to Checkout</button>
+                </>
+            }
+         
     </div>
     </div>
   )
